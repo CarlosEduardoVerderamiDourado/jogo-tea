fd1f87e5339a17c6d733abba2ed622861ebe110f
Liukass777 <lucasdrc28@gmail.com>
2025-05-21 17:25:13 -0300

    https://github.com/Liukass777/Rapeize.git

---
+++ mmg/livros/script/instance_objetcs.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@export var objects: Array[PackedScene]
+
+var i = 0
+var time = 0
+var time_limit = 1
+var pos = 0
+var pontos = 0
+
+func _ready():
+	i = randi_range(0,5)
+	pos = randf_range(50,520)
+
+func _process(delta):
+	time += delta
+	if time > time_limit:
+		create_objects()
+		time = 0
+	if pontos >= 100 : 
+		voltar_faze1()
+	$pontos.text = "PEGUE SOMENTE OS LIVROS QUE MARIA PEDIU: " + str(pontos)
+
+
+
+func create_objects():
+	var obj = objects[i].instantiate()
+	obj.position.x = pos
+	obj.position.y = -50
+	obj.scale = Vector2(0.2,0.2)
+	add_child(obj)
+	i = randi_range(0,5)
+	pos = randf_range(50,520)
+	time_limit = randf_range(0.5,1.5)
+	
+func voltar_faze1():
+	get_tree().change_scene_to_file("res://cenas/fase1.tscn")
---
+++ scripts/npc.gd
@@ -0,0 +1,75 @@
+extends Area2D
+
+@onready var laber_interacao: Label =$Labelinteracao
+@onready var caixa_dialogo: Label = $CanvasLayer/caixa_dialogo
+@onready var texto_dialogo: Label = $CanvasLayer/texto_Dialogo
+
+var player_in_area=false
+var falando = false
+var pode_avansar = false
+var fala_index= 0
+
+var falas = [
+	"Ola pode me ajudar...",
+	"pressiso de ajuda"
+]
+
+func _ready() -> void:
+	caixa_dialogo.visible = false
+	texto_dialogo.visible = false
+	laber_interacao.visible = false
+	
+
+func _process(_delta)->void:
+	if player_in_area and not falando and Input.is_action_just_pressed("interact"):
+		iniciar_dialogo()
+	elif falando and pode_avansar and Input.is_action_just_pressed("interact"):
+		proxima_fala()
+
+func _on_body_entered(body) -> void:
+	if body.name =="player":
+		player_in_area = true
+		laber_interacao.text = "Pressione 'E' para interagir"
+		laber_interacao.visible = true
+		
+
+func _on_body_exited(body) -> void:
+	if body.name =="player":
+		player_in_area = false
+		laber_interacao.visible = false
+		if falando:
+			encerrar_dialogo()
+
+func iniciar_dialogo():
+	falando = true
+	laber_interacao.visible= false
+	caixa_dialogo.visible = true
+	texto_dialogo.visible = true
+	fala_index = 0
+	proxima_fala()
+
+
+func proxima_fala():
+	if fala_index < falas.size():
+		pode_avansar = false
+		texto_dialogo.text = ""
+		var texto = falas[fala_index]
+		fala_index += 1
+		mostrar_texto_com_efeito(texto)
+	else:
+		encerrar_dialogo()
+
+func mostrar_texto_com_efeito(texto: String):
+	await get_tree().create_timer(0.1).timeout
+	for letra in texto:
+		texto_dialogo.text += letra
+	await get_tree().create_timer(0.02).timeout
+	pode_avansar = true
+
+
+func encerrar_dialogo():
+	falando = false
+	pode_avansar = false
+	caixa_dialogo.visible = false
+	texto_dialogo.visible = false
+	get_tree().change_scene_to_file("res://mmg/livros/cenas/fase.tscn")
